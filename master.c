c6c18b975d9d4a37efd422ca6738bd283cd5b426
aben20807 <aben20807@gmail.com>
2017-11-30 23:52:25 +0800

    Add default and ? case

--- master.c
+++ master.c
@@ -2,22 +2,38 @@
 
 int main(int argc, char **argv)
 {
-    printf("master start...\n");
-    char arg;
-    while((arg = getopt(argc, argv, "q:d:s:"))!= -1) {
-        switch(arg) {
-            case 'q':
-                printf("option q:'%s'\n",optarg);
-                break;
-            case 'd':
-                printf("option d:'%s'\n",optarg);
-                break;
-            case 's':
-                printf("option s:'%s'\n",optarg);
-                break;
-        }
-    }
-    printf("master finished\n\n");
+	printf("master start...\n\n");
+	char *query_word = NULL;
+	char *directory = NULL;
+	int num_slave = 1;
+	char arg;
+	while ((arg = getopt(argc, argv, "q:d:s:")) != -1) {
+		switch (arg) {
+		case 'q':
+			query_word = optarg;
+			break;
+		case 'd':
+			directory = optarg;
+			break;
+		case 's':
+			num_slave = atoi(optarg);
+			break;
+		case '?':
+			if (optopt == 'c')
+				fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+			else if (isprint (optopt))
+				fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+			else
+				fprintf(stderr, "Unknown option character `\\x%x'.\n", optopt);
+			return 1;
+		default:
+			abort();
+		}
+	}
+	printf("query_word: %s\n", query_word);
+	printf("directory: %s\n", directory);
+	printf("num_slave: %d\n", num_slave);
+	printf("\nmaster finished\n\n");
 }
 
 int send_to_fd(int sysfs_fd, struct mail_t *mail)
@@ -28,13 +44,13 @@ int send_to_fd(int sysfs_fd, struct mail_t *mail)
 
 	// int ret_val = write(sysfs_fd, ...);
 	// if (ret_val == ERR_FULL) {
-		/*
-		 * write something or nothing
-		 */
+	/*
+	 * write something or nothing
+	 */
 	// } else {
-		/*
-		 * write something or nothing
-		 */
+	/*
+	 * write something or nothing
+	 */
 	// }
 
 	/*
@@ -50,13 +66,13 @@ int receive_from_fd(int sysfs_fd, struct mail_t *mail)
 
 	// int ret_val = read(sysfs_fd, ...);
 	// if (ret_val == ERR_EMPTY) {
-		/*
-		 * write something or nothing
-		 */
+	/*
+	 * write something or nothing
+	 */
 	// } else {
-		/*
-		 * write something or nothing
-		 */
+	/*
+	 * write something or nothing
+	 */
 	// }
 
 	/*
--- master.h
+++ master.h
@@ -3,6 +3,8 @@
 
 #include "mail.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <getopt.h>
+#include <ctype.h>
 
 #endif
