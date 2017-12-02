33c18ffebb017dec54b963e86e7e70b9da428bb4
Huang Po-Hsuan <aben20807@gmail.com>
2017-12-02 11:27:57 +0800

    Clean code

--- master.c
+++ master.c
@@ -33,32 +33,6 @@ int main(int argc, char **argv)
 	printf("query_word: %s\n", query_word);
 	printf("directory: %s\n", directory);
 	printf("num_slave: %d\n", num_slave);
-
-	// fullname_queue = NULL;
-	//     DIR *dir = opendir(directory);
-	//     if (dir == NULL) {
-	//         char *err_msg = NULL;
-	//         CALLOC(err_msg, ERRMSG_SIZE, sizeof(char));
-	//         sprintf(err_msg, "opendir(%s)", directory);
-	//         perror(err_msg);
-	//         FREE(err_msg);
-	//         // return NULL;
-	//         exit(1);
-	//     }
-	//     struct dirent *entry;
-	//     while ((entry = readdir(dir)) != NULL) {
-	//         char *filename = NULL;
-	//         CALLOC(filename, 255, sizeof(char));
-	//         filename = entry->d_name;
-	//         char *full_path = NULL;
-	//         CALLOC(full_path, 255, sizeof(char));
-	//         realpath(filename, full_path);
-	//         printf("%d\n", (entry->d_type == DT_DIR));
-	//         // printf("%s\n", filename);
-	//         printf("%s\n", full_path);
-	//     }
-	//     closedir(dir);
-
 	// listdir(directory, 0);
 	Queue *q = NULL;
 	init(&q);
@@ -66,9 +40,6 @@ int main(int argc, char **argv)
 	printf("%d\n", q->size(q));
 	mail_t m = {{"a"}, "p"};
 	printf("%s, %s\n", m.data.query_word, m.file_path);
-	// mail_t *mm = NULL;
-	// CALLOC(mm, sizeof(mail_ptr), 1);
-	// strncpy(mm->data.query_word, "a", sizeof(mm->data.query_word));
 	q->enq(q, &m);
 	printf("%d\n", q->size(q));
 	node_ptr p = q->deq(q);
@@ -102,23 +73,10 @@ void enq(Queue *self, node_ptr item)
 		self->tail = item;
 	}
 	self->count++;
-	// node_ptr tmp = NULL;
-	// MALLOC(tmp, sizeof(struct node));
-	// tmp->mail = *mail;
-	// self->head->prev = tmp;
-	// self->head = self->head->prev;
 }
 
 node_ptr deq(Queue *self)
 {
-	// mail_ptr m = NULL;
-	// MALLOC(m, sizeof(mail_ptr));
-	// *m = self->tail->mail;
-	// node_ptr tmp = self->tail;
-	// self->tail = self->tail->prev;
-	// self->tail->next = NULL;
-	// FREE(tmp);
-	// return m;
 	node_ptr tmp = self->tail;
 	self->tail = self->tail->prev;
 	self->count--;
