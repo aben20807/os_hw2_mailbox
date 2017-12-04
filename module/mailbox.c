532627c270db6ad9077156b1a99ab7c1e6d233ad
Huang Po-Hsuan <aben20807@gmail.com>
2017-12-04 15:57:33 +0800

    Can add mail into list, test by list_for_each

--- module/mailbox.c
+++ module/mailbox.c
@@ -31,21 +31,27 @@ static ssize_t mailbox_read(struct kobject *kobj,
 static ssize_t mailbox_write(struct kobject *kobj,
                              struct kobj_attribute *attr, const char *buf, size_t count)
 {
-	// mailbox_entry_t *tmp = NULL; // tmp = kmalloc(sizeof(mailbox_entry_t), GFP_KERNEL);
-	// tmp->mail_p = kmalloc(sizeof(mailbox_entry_t), GFP_KERNEL);
-	// // sscanf(buf, "%du", &tmp->mail_p);
-	// memcpy(tmp, buf, sizeof(char) * count);
-	// list_add(&tmp->entry, &m_head->head);
-	// mail_t *m = (mail_t *)kobj;
-	// mail_t *m = NULL;
-	// m = to_mail(kobj);
-	mail_t *m = kmalloc(sizeof(struct mail_t), GFP_KERNEL);
-	// printk("call mailbox_write %ld: %s\n", count, buf);
-	// printk("buf siz %ld\n", PAGE_SIZE);
-	// m = (struct mail_t *)buf;
-	memcpy(m, buf - 64, count);
+	mail_t *m;
+	struct list_head *iter;
+	mailbox_entry_t *tmp;
+	if (m_head == NULL) {
+		m_head = kmalloc(sizeof(struct mailbox_head_t), GFP_KERNEL);
+		INIT_LIST_HEAD(&m_head->head);
+	}
+	m = kmalloc(sizeof(struct mail_t), GFP_KERNEL);
+	memcpy(m, buf - 64, count); // mysterious power number: 64
 	printk("mail: %s, %s\n", m->data.query_word, m->file_path);
-	// printk("call mailbox_write %s\n", m->file_path);
+	if (m != NULL) {
+		mailbox_entry_t *m_entry = kmalloc(sizeof(struct mailbox_entry_t), GFP_KERNEL);
+		m_entry->mail_p = m;
+		list_add(&m_entry->entry, &m_head->head);
+	}
+	printk("In list:\n");
+	list_for_each(iter, &m_head->head) {
+		tmp = list_entry(iter, struct mailbox_entry_t, entry);
+		printk("%s\n", tmp->mail_p->file_path);
+	}
+	printk("\n");
 	// return ERR_FULL;
 	return count;
 }
@@ -59,6 +65,7 @@ static int __init mailbox_init(void)
 	// m_head = kmalloc(sizeof(mailbox_head_t), GFP_KERNEL);
 	// m_head->head =
 	// INIT_LIST_HEAD(&m_head->head);
+	m_head = NULL;
 	return 0;
 }
 
